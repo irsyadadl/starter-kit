fd54a9cdb3c52c71e669849b806deafd101eb122
Irsyad A. Panjaitan <irsyad@parsinta.com>
2025-07-22 05:10:43 +0700

    wip

---
+++ src/components/theme-provider.tsx
@@ -0,0 +1,19 @@
+"use client"
+
+import { ThemeProvider as Primitive, type ThemeProviderProps, useTheme } from "next-themes"
+
+const ThemeProvider = ({ children, ...props }: ThemeProviderProps) => {
+  return (
+    <Primitive
+      storageKey="primitive-theme"
+      enableSystem
+      disableTransitionOnChange
+      attribute="class"
+      {...props}
+    >
+      {children}
+    </Primitive>
+  )
+}
+
+export { ThemeProvider, useTheme }
